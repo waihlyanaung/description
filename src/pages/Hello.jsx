a0d3095b99ecb32c2d4efc74dfe0b7aa360b9670
waihlyanaung-lastoenjoy <wailyanaung.lastoenjoy@gmail.com>
2023-11-15 19:23:39 +0630

    v

--- src/pages/Hello.jsx
+++ src/pages/Hello.jsx
@@ -1,17 +1,36 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+// Hello.jsx
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { Helmet } from 'react-helmet';
 
 const Hello = () => {
+  const [metaTags, setMetaTags] = useState({
+    title: 'Hello Page',
+    description: 'This is the Hello page description.',
+  });
+
+  useEffect(() => {
+    // Update meta tags when the component mounts
+    setMetaTags({
+      title: 'Hello Page',
+      description: 'This is the Hello page description.',
+    });
+  }, []);
+
   return (
     <div>
-        <Link to="/">
+      <Helmet>
+        <title>{metaTags.title}</title>
+        <meta name="description" content={metaTags.description} />
+      </Helmet>
+      <Link to="/">
         <button className="bg-red-500 hover:bg-blue-700 ml-11 text-white font-bold py-2 px-4 rounded mt-4">
           Rev Page
         </button>
       </Link>
-        Hello
-        </div>
-  )
-}
+      Hello
+    </div>
+  );
+};
 
-export default Hello
+export default Hello;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
-import React from "react";
-import { Link } from "react-router-dom";
+// Home.jsx
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
   return (

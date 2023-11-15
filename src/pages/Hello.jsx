03174a02154974ae8202a8fb53e2fbc079e2ba15
waihlyanaung-lastoenjoy <wailyanaung.lastoenjoy@gmail.com>
2023-11-15 19:03:38 +0630

    v2

--- src/pages/Hello.jsx
+++ src/pages/Hello.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Hello = () => {
   return (
-    <div>Hello</div>
+    <div>
+        <Link to="/">
+        <button className="bg-red-500 hover:bg-blue-700 ml-11 text-white font-bold py-2 px-4 rounded mt-4">
+          Rev Page
+        </button>
+      </Link>
+        Hello
+        </div>
   )
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,20 +1,18 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
-
     <div>
-   <div className="font-pacifico">
-What is Lorem Ipsum?
-
-   </div>
-   <div className="mt-9 font-saira-condensed">
-   Lorem Ipsum is 
-
-   </div>
-   
-   </div> 
- );
+      <Link to="/hello">
+        <button className="bg-red-500 hover:bg-blue-700 ml-11 text-white font-bold py-2 px-4 rounded mt-4">
+          Next Page
+        </button>
+      </Link>
+      <div className="font-pacifico">What is Lorem Ipsum?</div>
+      <div className="mt-9 font-saira-condensed">Lorem Ipsum is</div>
+    </div>
+  );
 };
 
 export default Home;
